bde7b2da45e926b733c9017fb39bf448bc4808a4
andersonsimplicio <anderson25eu@gmail.com>
2026-07-27 16:37:02 -0300

    Ajuste direction

--- Assets/Scenes/Scripts/Player/Player.cs
+++ Assets/Scenes/Scripts/Player/Player.cs
@@ -1,28 +1,41 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour
 {
 
     private Vector2 direction;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private float speed = 5f;
+    private Rigidbody2D rig; 
+
     void Start()
     {
-        direction = new Vector2();
+        this.direction = Vector2.zero;
+        rig = GetComponent<Rigidbody2D>();
+    }
+    
+   void Update()
+    {
+      
     }
 
-    // Update is called once per frame
-    void Update()
+    private void FixedUpdate()
     {
-        
+         rig.linearVelocity =Mover() * (speed);    
     }
 
-    void FixedUpdate()
+    Vector2 Mover()
     {
-        
+         this.direction = Vector2.zero;
+        if (Keyboard.current.wKey.isPressed || Keyboard.current.upArrowKey.isPressed) 
+            this.direction.y = 1f;
+        if (Keyboard.current.sKey.isPressed || Keyboard.current.downArrowKey.isPressed) 
+            this.direction.y = -1f;
+        if (Keyboard.current.aKey.isPressed || Keyboard.current.leftArrowKey.isPressed) 
+            this.direction.x = -1f;
+        if (Keyboard.current.dKey.isPressed || Keyboard.current.rightArrowKey.isPressed) 
+            this.direction.x = 1f;
+        return this.direction.normalized;
     }
-
-    private Vector2 Move()
-    {
-        return new Vector2(0,0);
-    }
+  
 }
