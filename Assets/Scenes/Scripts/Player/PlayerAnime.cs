1383980de0461775b4b5a2fc31609035474d57f9
andersonsimplicio <anderson25eu@gmail.com>
2026-07-28 17:22:31 -0300

    Animações walking, run e idle prontas

--- Assets/Scenes/Scripts/Player/PlayerAnime.cs
+++ Assets/Scenes/Scripts/Player/PlayerAnime.cs
@@ -12,19 +12,35 @@ public class PlayerAnime : MonoBehaviour
         player = GetComponent<Player>();
         animator = GetComponent<Animator>();
     }
-
     public void Update()
+    {
+        OnMove();
+        OnRun();
+    }
+
+  
+
+    void OnMove()
     {
         if (player._direction.sqrMagnitude > 0){             
             animator.SetInteger(TransicaoHash, 1);
         }else{
             animator.SetInteger(TransicaoHash, 0);
         }
-        
-        if (player._direction.x > 0){             
+         if (player._direction.x > 0){             
           transform.eulerAngles = new Vector2(0,0);
         }else if (player._direction.x < 0){
             transform.eulerAngles = new Vector2(0, 180);
         }
+        
     }
+    void OnRun()
+    {
+        if (player._isRunning && player._direction.sqrMagnitude > 0)
+        {   
+             animator.SetInteger(TransicaoHash, 2);
+        }
+    }
+
+    
 }
