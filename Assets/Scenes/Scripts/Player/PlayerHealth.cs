abc48152a4bebcda0fd9e949a908e571e55a5ab1
andersonsimplicio <anderson25eu@gmail.com>
2026-07-30 15:41:10 -0300

    Atualizaçao

--- Assets/Scenes/Scripts/Player/PlayerHealth.cs
+++ Assets/Scenes/Scripts/Player/PlayerHealth.cs
@@ -1,22 +1,23 @@
+using TMPro;
 using UnityEngine;
 
 [RequireComponent(typeof(Player))]
 public class PlayerHealt : MonoBehaviour
 {
     private Player player;
-    
+    [SerializeField] TMP_Text healthText;
     [SerializeField] int maxtHelt;
     void Start()
     {
         player = GetComponent<Player>();
         maxtHelt = player._health;
-        //healttex.text = "HP: "+ player._health +" / "+maxtHelt;
+        healthText.text = "HP: "+ player._health +" / "+maxtHelt;
     }
     
     public void ChanngeHealth(int amount)
     {
         player._health +=amount;
-        //healttex.text = "HP: "+ player._health +" / "+maxtHelt;
+        //healthText.text = "HP: "+ player._health +" / "+maxtHelt;
         if(player._health <= 0)
         {
             player.gameObject.SetActive(false);
