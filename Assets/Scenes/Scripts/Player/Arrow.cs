abc302c5a5f0befc910b109cd93cb978e8103916
andesonsimplicio <andeson25eu@gmail.com>
2026-09-10 19:01:47 -0300

    Arrow 9:29

--- Assets/Scenes/Scripts/Player/Arrow.cs
+++ Assets/Scenes/Scripts/Player/Arrow.cs
@@ -1,15 +1,27 @@
 using UnityEngine;
 
-public class Arrow : MonoBehaviour
-{
+public class Arrow : MonoBehaviour{
     [SerializeField] private Rigidbody2D rb;
     [SerializeField] private Vector2 direction = Vector2.right;
-    [SerializeField] private float lifeSpaw = 2;    
-    [SerializeField] private float speed = 2;    
-    void Start()
+    [SerializeField] private float lifeSpan = 2f;
+    [SerializeField] private float speed = 2f;
+
+    public void SetDirection(Vector2 newDirection)
     {
-        rb.linearVelocity = speed*direction;
-        Destroy(gameObject,lifeSpaw);
+        direction = newDirection.normalized;
+
+        float angle = Mathf.Atan2(
+            direction.y,
+            direction.x
+        ) * Mathf.Rad2Deg;
+
+        transform.rotation = Quaternion.Euler(0, 0, angle);
+    }
+
+    private void Start()
+    {
+        rb.linearVelocity = speed * direction;
+        Destroy(gameObject, lifeSpan);
     }
 
 }
--- Assets/Scenes/Scripts/Player/PlayerBow.cs
+++ Assets/Scenes/Scripts/Player/PlayerBow.cs
@@ -1,27 +1,63 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerBow : MonoBehaviour
 {
     [SerializeField] private Transform launchPoint;
     [SerializeField] private GameObject arrowPrefab;
+    [SerializeField] private Vector2 aimDirection = Vector2.right;
 
+  
 
-
-
-
-    void Start()
+    private void Update()
     {
-        
+        HandheldAiming();
+
+        if (Keyboard.current != null &&
+            Keyboard.current.cKey.wasPressedThisFrame)
+        {
+            Shoot();
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void HandheldAiming()
     {
-        Shoot();
+        Vector2 input = Vector2.zero;
+
+        if (Keyboard.current == null)
+            return;
+
+        if (Keyboard.current.aKey.isPressed)
+            input.x = -1;
+
+        if (Keyboard.current.dKey.isPressed)
+            input.x = 1;
+
+        if (Keyboard.current.wKey.isPressed)
+            input.y = 1;
+
+        if (Keyboard.current.sKey.isPressed)
+            input.y = -1;
+
+        if (input != Vector2.zero)
+        {
+            aimDirection = input.normalized;
+
+            // Move o LaunchPoint na direção da mira
+           launchPoint.localPosition =  (Vector3)aimDirection * 1.0f;
+        }
     }
 
-    public void Shoot()
+    private void Shoot()
     {
-        Instantiate(arrowPrefab,launchPoint.position,Quaternion.identity);
+     GameObject arrowObject = Instantiate(
+        arrowPrefab,
+        launchPoint.position,
+        Quaternion.identity
+    );
+
+    Arrow arrow = arrowObject.GetComponent<Arrow>();
+
+    arrow.SetDirection(aimDirection);
     }
 }
