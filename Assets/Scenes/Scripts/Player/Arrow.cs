c7298b067a637b1100301465c5106ff56ca9b805
andesonsimplicio <andeson25eu@gmail.com>
2026-09-14 18:46:08 -0300

    Configurando flecha

--- Assets/Scenes/Scripts/Player/Arrow.cs
+++ Assets/Scenes/Scripts/Player/Arrow.cs
@@ -14,8 +14,11 @@ public class Arrow : MonoBehaviour{
     [SerializeField] private Rigidbody2D rb;
     [SerializeField] private Vector2 direction = Vector2.right;
     [SerializeField] private LayerMask enemyLayer;
+    [SerializeField] private LayerMask obstacleLayer;
+    [SerializeField] private SpriteRenderer sr;
+    [SerializeField] private Sprite buriedSprite;
 
-
+    
 
 
     public void SetDirection(Vector2 newDirection)
@@ -38,12 +41,11 @@ public class Arrow : MonoBehaviour{
 
     public void OnCollisionEnter2D(Collision2D collision)
     {
-        Debug.LogWarning( $" Saude: {collision.gameObject.GetComponent<EnemyHeath>().Health}");
 
         if ( (enemyLayer.value & (1<< collision.gameObject.layer)) > 0){
             collision.gameObject.GetComponent<EnemyHeath>().ChangeHealth(-damage);
             collision.gameObject.GetComponent<EnemyKnowBack>().knowBack(transform,knockBackForce,knockBackTime,stunTime);
-            Debug.LogWarning( $" Saude: {collision.gameObject.GetComponent<EnemyHeath>().Health}");
+
         }
         
     }
